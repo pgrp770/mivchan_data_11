5f7a58b93dbfad00f4835356f2657eae3a7c6b88
pgrp770 <pgrp770@gmail.com>
2024-11-14 15:22:20 +0200

    feat: add suspicions route

--- main_producer/app/main.py
+++ main_producer/app/main.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
 from main_producer.app.routes.message_route import message_blueprint
+from main_producer.app.routes.suspicions_route import suspicions_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(message_blueprint, url_prefix="/api/email")
+app.register_blueprint(suspicions_blueprint, url_prefix="/api/suspicions")
 
 if __name__ == '__main__':
     app.run()
--- main_producer/app/routes/suspicions_route.py
+++ main_producer/app/routes/suspicions_route.py
@@ -4,10 +4,12 @@ from main_producer.app.kafka_producer.all_message_producer import produce_all_me
 from main_producer.app.kafka_producer.explosive_message_producer import produce_explosive_messages
 from main_producer.app.kafka_producer.hostage_message_producer import produce_hostage_messages
 from main_producer.app.service.producers_service import organize_messages_list
+from main_producer.app.service.suspicion_service import get_list_of_all_sentences
 
 suspicions_blueprint = Blueprint('suspicions_bluprint', __name__)
 
 
-# @suspicions_blueprint.route('/sentences_by_email', methods=['GET'])
-# def get_suspicions_message_by_email():
-
+@suspicions_blueprint.route('/sentences_by_email/<string:email>', methods=['GET'])
+def get_suspicions_message_by_email(email):
+    sentences = get_list_of_all_sentences(email)
+    return jsonify({"sentences": sentences}), 200
