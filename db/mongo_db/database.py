0bef1c300631d1656027825a7eac62a4a9242f28
pgrp770 <pgrp770@gmail.com>
2024-11-14 10:20:41 +0200

    feat: connect to mongo

---
+++ db/mongo_db/database.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+
+client = MongoClient(os.environ["MONGO_URL"])
+
+messages_db = client["gym"]
+
+all_messages_collection = messages_db["all_messages_collection"]
