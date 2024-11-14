eaf4750496e78a91ccfe3797b6eecb1b87e77a86
pgrp770 <pgrp770@gmail.com>
2024-11-14 15:18:58 +0200

    feat: add suspicion_service to the main producer

--- main_producer/app/service/suspicion_service.py
+++ main_producer/app/service/suspicion_service.py
@@ -1,1 +1,14 @@
+from toolz import pipe
+from toolz.curried import partial
+
+from main_producer.app.repository.senetences_repository import *
+
+
 def get_list_of_all_sentences(email):
+    sentences_hostage = get_sentences_hostage_by_email(email)
+    sentences_explosive = get_sentences_explosive_by_email(email)
+    return pipe(
+        sentences_hostage + sentences_explosive,
+        partial(map, lambda sentence: sentence.sentence),
+        list
+    )
